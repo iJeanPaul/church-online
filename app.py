c0210f26d315f8e97d7158a9a2dc72abc7290486
Jean Paul Iradukunda <jp.iradukunda@pesachoice.com>
2017-01-11 21:50:30 -0600

    more initial code cleanup

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ from flask_jwt import JWT
 
 from security import authenticate, identity
 from resources.user import UserRegister, UserList
-#from resources.item import Item, ItemList
-#from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
@@ -16,13 +14,6 @@ app.secret_key = 'chuch-online-api'
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity)  # /auth
-
-'''
-api.add_resource(Store, '/store/<string:name>')
-api.add_resource(Item, '/item/<string:name>')
-api.add_resource(ItemList, '/items')
-api.add_resource(StoreList, '/stores')
-'''
 
 api.add_resource(UserRegister, '/register')
 api.add_resource(UserList, '/users')
